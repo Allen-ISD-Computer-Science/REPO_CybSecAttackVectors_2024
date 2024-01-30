c013dffa0da317b1a9066ff6e1a6a8d389b142fd
R0H1T-RAJU <72674397+R0H1T-RAJU@users.noreply.github.com>
2024-01-30 12:16:03 -0600

    added basic IOS application

---
+++ AttackVectors/AttackVectors/AttackVectorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttackVectorsApp.swift
+//  AttackVectors
+//
+//  Created by Rohit Bhupathiraju on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AttackVectorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
