bf45403a3b7f2435070eaff24b39ae566b3a1cb0
KemoLito <111138527+KemoLito@users.noreply.github.com>
2024-02-27 12:12:14 -0600

    java code for spoofing on android
    allows a mock broadcast receiver to change your location

---
+++ MockLocationProvider.java
@@ -0,0 +1,98 @@
+package cl.coders.faketraveler;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.SystemClock;
+
+public class MockLocationProvider {
+    String providerName;
+    Context ctx;
+
+    /**
+     * Class constructor
+     *
+     * @param name provider
+     * @param ctx  context
+     * @return Void
+     */
+    public MockLocationProvider(String name, Context ctx) {
+        this.providerName = name;
+        this.ctx = ctx;
+
+        int powerUsage = 0;
+        int accuracy   = 5;
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            powerUsage = 1;
+            accuracy   = 2;
+        }
+
+        LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+        startup(lm, powerUsage, accuracy, /* maxRetryCount= */ 3, /* currentRetryCount= */ 0);
+    }
+
+    private void startup(LocationManager lm, int powerUsage, int accuracy, int maxRetryCount, int currentRetryCount) {
+        if (currentRetryCount < maxRetryCount) {
+            try {
+                shutdown();
+                lm.addTestProvider(providerName, false, false, false, false, false, true, true, powerUsage, accuracy);
+                lm.setTestProviderEnabled(providerName, true);
+            }
+            catch(Exception e) {
+                startup(lm, powerUsage, accuracy, maxRetryCount, (currentRetryCount + 1));
+            }
+        }
+        else {
+            throw new SecurityException("Not allowed to perform MOCK_LOCATION");
+        }
+    }
+
+    /**
+     * Pushes the location in the system (mock). This is where the magic gets done.
+     *
+     * @param lat latitude
+     * @param lon longitude
+     * @return Void
+     */
+    public void pushLocation(double lat, double lon) {
+        LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+
+        Location mockLocation = new Location(providerName);
+        mockLocation.setLatitude(lat);
+        mockLocation.setLongitude(lon);
+        mockLocation.setAltitude(3F);
+        mockLocation.setTime(System.currentTimeMillis());
+        //mockLocation.setAccuracy(16F);
+        mockLocation.setSpeed(0.01F);
+        mockLocation.setBearing(1F);
+        mockLocation.setAccuracy(3F);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mockLocation.setBearingAccuracyDegrees(0.1F);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mockLocation.setVerticalAccuracyMeters(0.1F);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mockLocation.setSpeedAccuracyMetersPerSecond(0.01F);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            mockLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        }
+        lm.setTestProviderLocation(providerName, mockLocation);
+    }
+
+    /**
+     * Removes the provider
+     *
+     * @return Void
+     */
+    public void shutdown() {
+        try {
+            LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+            lm.removeTestProvider(providerName);
+        }
+        catch(Exception e) {}
+    }
+}
