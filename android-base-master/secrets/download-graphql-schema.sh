2bbc7de293a3464cacc7c4e6cffd1f9b94d128f5
R0H1T-RAJU <72674397+R0H1T-RAJU@users.noreply.github.com>
2024-03-22 20:44:53 -0500

    Delete android-base-master directory

--- android-base-master/secrets/download-graphql-schema.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./gradlew downloadApolloSchema -Pcom.apollographql.apollo.endpoint=https://rails-base-graphql-api.herokuapp.com/graphql -Pcom.apollographql.apollo.schema=src/main/graphql/com/flatstack/android/schema.json
